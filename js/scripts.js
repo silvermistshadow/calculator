89cac699a4bea527fedc6426a4c8284970d2c2f1
silvermistshadow <bmead72@gmail.com>
2019-02-11 13:07:47 -0800

    add multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -33,5 +33,11 @@ $(document).ready(function() {
     var result = subtract(number1, number2);
     $("#output").text(result);
   });
-
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#multi1").val());
+    var number2 = parseInt($("#multi2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
 });
