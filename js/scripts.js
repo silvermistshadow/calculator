d2169608bff25b81d1b3ef4b4ce26c604144aa13
silvermistshadow <bmead72@gmail.com>
2019-02-13 09:28:31 -0800

    add divide in html and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -40,4 +40,12 @@ $(document).ready(function() {
     var result = multiply(number1, number2);
     $("#output").text(result);
   });
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  })
+
 });
