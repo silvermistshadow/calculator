dbff3ce05285dabfd1a7a1fff7b6a3cb27c7c403
silvermistshadow <bmead72@gmail.com>
2019-03-06 11:34:13 -0800

    switch to branching in javascript, remake html to use a single form

--- js/scripts.js
+++ js/scripts.js
@@ -19,33 +19,26 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    }
+    else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    else {
+      console.log("FLAGRANT SYSTEM ERROR" + " " + "The system is down. I dunno what you did, moron, but you sure screwed everything up good.")
+    }
     $("#output").text(result);
   });
-  $("form#subtract").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#multiply").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#multi1").val());
-    var number2 = parseInt($("#multi2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#divide").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
-  })
 
 });
